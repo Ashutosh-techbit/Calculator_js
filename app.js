11e157a87de04f9fca8636997ab4d146ee831910
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-11-13 18:48:45 +0530

    Calculator using js

---
+++ app.js
@@ -0,0 +1,27 @@
+let btn = document.querySelectorAll(".num");
+let op= document.querySelectorAll(".op");
+let str="";
+
+for (nums of btn) {
+  nums.addEventListener("click", (event) => {
+    if(event.target.innerText == "="){
+      // usng eval function
+        str= eval (str);
+        document.querySelector("input").value=str;
+    }
+    else if(event.target.innerText == "clr"){
+        str="";
+        document.querySelector("input").value=str;
+    }
+    else{
+    console.dir(event.target.innerText);
+    str+=event.target.innerText;
+    document.querySelector("input").value=str;
+    }
+        
+  });
+}
+
+
+
+
